056376e0cb4ab86afca76c9a9bec8a1bf5632289
marvinlab <m.labrador@klab.com>
2014-10-22 16:19:37 +0800

    Created:  Empty Night Watch Project

---
+++ NightWatch/NightWatch/NWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NWAppDelegate.h
+//  NightWatch
+//
+//  Created by Marvin Labrador on 10/22/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NightWatch/NightWatch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NightWatch
+//
+//  Created by Marvin Labrador on 10/22/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NWAppDelegate class]));
+    }
+}
