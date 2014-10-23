91a9245b50f71e780902ff2d20d2b4af3b42460e
marvinlab <m.labrador@klab.com>
2014-10-23 12:27:17 +0800

    Cross created, responds to touch, follows touch

---
+++ NightWatch/NightWatch/NWCross.h
@@ -0,0 +1,13 @@
+//
+//  NWCross.h
+//  NightWatch
+//
+//  Created by Marvin Labrador on 10/23/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWCross : UIImageView
+
+@end
---
+++ NightWatch/NightWatch/NWCross.m
@@ -0,0 +1,31 @@
+//
+//  NWCross.m
+//  NightWatch
+//
+//  Created by Marvin Labrador on 10/23/14.
+//  Copyright (c) 2014 Marvin Labrador. All rights reserved.
+//
+
+#import "NWCross.h"
+
+@implementation NWCross
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
